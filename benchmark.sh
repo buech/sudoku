3c3ea168ef6f4096c38d099c5410d1d1d78055cc
Adam Buechner <buechner.adam@gmx.de>
2019-01-13 13:03:40 +0100

    Remove unnecessary cat

--- benchmark.sh
+++ benchmark.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env sh
 
 printf "Running C-solver (backtracking) ...\n"
-time cat top95.txt | sed 's/\./0/g' | xargs -L1 ./solver -b bt > /dev/null
+time sed 's/\./0/g' top95.txt | xargs -L1 ./solver -b bt > /dev/null
 
 printf "Running Fortran-solver ...\n"
-time cat top95.txt | sed 's/\./0/g' | xargs -L1 ./fsolver > /dev/null
+time sed 's/\./0/g' top95.txt | xargs -L1 ./fsolver > /dev/null
 
 printf "Running C-solver (dlx) ...\n"
-time cat top95.txt | sed 's/\./0/g' | xargs -L1 ./solver -b dlx > /dev/null
+time sed 's/\./0/g' top95.txt | xargs -L1 ./solver -b dlx > /dev/null
