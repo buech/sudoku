c80a5dd42b09d2e44fc5c4219a0c4992e4024d73
Adam Buechner <buechner.adam@gmx.de>
2019-01-15 13:01:37 +0100

    Minimize strlen calls

--- string_to_board.c
+++ string_to_board.c
@@ -2,10 +2,11 @@
 
 int string_to_board(int *board, char *string) {
    int err = 0;
-   if (strlen(string) < 81) {
+   size_t len = strlen(string);
+   if (len < 81) {
       return 1;
    } else {
-      if (strlen(string) > 81)
+      if (len > 81)
          err = 2;
       for (int i = 0; i < 81; i++)
          board[i] = string[i] >= '1' && string[i] <= '9' ? string[i]-'0' : 0;
