7b3c9b80e130d67acb61031d0af546c22cf61f14
Adam Buechner <buechner.adam@gmx.de>
2019-02-12 14:39:17 +0100

    Fix undefined behaviour

--- backend_backtracking.c
+++ backend_backtracking.c
@@ -10,16 +10,24 @@ static int search(int *board, int pos) {
    unsigned short valid = 0;
 
    for (int i = 0; i < 9; ++i) {
-      valid |= 1 << (board[9 * row + i] - 1);
-      valid |= 1 << (board[9 * i + col] - 1);
+      int a = board[9 * row + i];
+      int b = board[9 * i + col];
+      if (a)
+         valid |= 1 << (a - 1);
+      if (b)
+         valid |= 1 << (b - 1);
    }
 
    col = col / 3 * 3;
    row = row / 3 * 3;
 
-   for (int i = row; i < row + 3; ++i)
-      for (int j = col; j < col + 3; ++j)
-         valid |= 1 << (board[9 * i + j] - 1);
+   for (int i = row; i < row + 3; ++i) {
+      for (int j = col; j < col + 3; ++j) {
+         int a = board[9 * i + j];
+         if (a)
+            valid |= 1 << (a - 1);
+      }
+   }
 
    valid = ~valid;
 
--- backend_dumb.c
+++ backend_dumb.c
@@ -12,17 +12,25 @@ static void fill(unsigned short *pool, int *board) {
       int row = pos / 9;
       unsigned short valid = 0;
 
-      for (int i = 0; i < 9; i++) {
-         valid |= 1 << (board[9 * row + i] - 1);
-         valid |= 1 << (board[9 * i + col] - 1);
+      for (int i = 0; i < 9; ++i) {
+         int a = board[9 * row + i];
+         int b = board[9 * i + col];
+         if (a)
+            valid |= 1 << (a - 1);
+         if (b)
+            valid |= 1 << (b - 1);
       }
 
       col = col / 3 * 3;
       row = row / 3 * 3;
 
-      for (int i = row; i < row + 3; i++)
-         for (int j = col; j < col + 3; j++)
-            valid |= 1 << (board[9 * i + j] - 1);
+      for (int i = row; i < row + 3; ++i) {
+         for (int j = col; j < col + 3; ++j) {
+            int a = board[9 * i + j];
+            if (a)
+               valid |= 1 << (a - 1);
+         }
+      }
 
       pool[pos] = ~valid & 0x1ff;
    }
